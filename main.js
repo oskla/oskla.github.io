c1da7882ce778add93460f38e2d2ea1a5a91c9ee
oskla <oskarlarssson@gmail.com>
2021-09-17 10:50:52 +0200

    Första test

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+//definierar variabler
+let list1 = document.getElementById("list1");
+let knappen = document.getElementById("btn1");
+
+
+//bildBoxParent.innerHTML =+ "<div id="+"bild"+i+">"+"hejhej"+"</div>"
+
+//function loop() {
+//    for (let i = 0; i < 5; i++) {
+//    
+    //    list1.innerHTML += "<li>hejhej"+i+"</li>"
+  //      
+   // }
+
+//}
+
+
+
+let pushBtn = knappen.addEventListener("click", function() {
+    
+    for (let i = 1; i < 5; i++) {
+    
+        list1.innerHTML += "<button>"+i+"</button>"
+        
+     }
+
+}
+);
+
+let ClearAll = document.getElementById("clear-all");
+
+
+   
+
+
+
+
+
+
+
