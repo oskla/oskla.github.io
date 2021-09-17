f2bde49c255a78f4cdc357d5fb1f64383d1a7318
oskla <oskarlarssson@gmail.com>
2021-09-17 17:30:15 +0200

    rename site+fontFix

--- main.js
+++ main.js
@@ -1,42 +1,35 @@
 
-
 //definierar variabler
 let list1 = document.getElementById("list1");
 let knappen = document.getElementById("btn1");
 let clearAll = document.getElementById("clear-all")
 
 
-///bildBoxParent.innerHTML =+ "<div id="+"bild"+i+">"+"hejhej"+"</div>"
 
-//function loop() {
-//    for (let i = 0; i < 5; i++) {
-//    
-    //    list1.innerHTML += "<li>hejhej"+i+"</li>"
-  //      
-   // }
-
-//}
-
-
-
+// vad ska hända när jag trycker på Btn1
 let pushBtn = knappen.addEventListener("click", function() {
     
     for (let i = 1; i < 5; i++) {
-    
-        list1.innerHTML += "<button>"+i+"</button>"
         
+      
+        list1.innerHTML += "<button>"+i+"</button>"
      }
+});
 
-}
-);
 
+//definierar ClearAll-knappen
 let ClearAll = document.getElementById("clear-all");
 
 
+//definera funktionen refreshPage - ladda om sidan + scrolla till toppen
 function refreshPage(){
     window.location.reload();
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+ 
 } 
 
+//utför ClearAll-funktionen
 clearAll.addEventListener("click",refreshPage);
 
 
